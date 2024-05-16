4dd9012aba96836396659e53dbb34dabfa28e976
Suganthi Krishnavathi <suganthikrishnavathi@MacBook-Pro.local>
2024-05-16 14:09:36 +1000

    Init

---
+++ src/main/java/io/suganthi/snykdemo/SnykDemoApplication.java
@@ -0,0 +1,13 @@
+package io.suganthi.snykdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SnykDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SnykDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/suganthi/snykdemo/SnykDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package io.suganthi.snykdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SnykDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
